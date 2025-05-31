cc0bf5c45d8b296bec68fdb673dd1f7ebb466a1a
sneakytowelsuit <60020423+sneakytowelsuit@users.noreply.github.com>
2025-05-31 10:33:04 -0700

    Refactor ConditionUtils to use ArrayList instead of LinkedList for path management; add unit tests for getIdPath method

--- src/main/java/com/github/sneakytowelsuit/purerules/utils/ConditionUtils.java
+++ src/main/java/com/github/sneakytowelsuit/purerules/utils/ConditionUtils.java
@@ -2,7 +2,7 @@ package com.github.sneakytowelsuit.purerules.utils;
 
 import com.github.sneakytowelsuit.purerules.conditions.Condition;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConditionUtils {
@@ -10,10 +10,10 @@ public class ConditionUtils {
     public static <T extends Condition<?>> List<String> getIdPath(T condition, List<String> currentPath) {
         List<String> path;
         if (currentPath == null) {
-           path = new LinkedList<>();
+           path = new ArrayList<>();
         }
         else {
-            path = new LinkedList<>(currentPath);
+            path = new ArrayList<>(currentPath);
         }
         path.add(condition.getId());
         return path;
