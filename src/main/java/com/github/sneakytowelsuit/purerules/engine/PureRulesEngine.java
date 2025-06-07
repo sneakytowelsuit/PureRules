c8ba5eb708a44dde99f9aa8be6ad0429143307e3
sneakytowelsuit <60020423+sneakytowelsuit@users.noreply.github.com>
2025-06-06 20:33:46 -0700

    Enhance PureRulesEngine: introduce probabilistic mode with minimum probability threshold; refactor constructors for better clarity

--- src/main/java/com/github/sneakytowelsuit/purerules/engine/PureRulesEngine.java
+++ src/main/java/com/github/sneakytowelsuit/purerules/engine/PureRulesEngine.java
@@ -8,18 +8,47 @@ import java.util.List;
 public class PureRulesEngine<TInput> {
   private final EngineContext context;
   private final List<RuleGroup<TInput>> ruleGroups;
-  private EngineMode engineMode = EngineMode.DETERMINISTIC;
+  /**
+   * The mode of the engine, which can be either DETERMINISTIC or PROBABILISTIC.
+   * This determines how the rules are evaluated and the results are combined.
+   * <ul>
+   *     <li>DETERMINISTIC: The engine evaluates rules and returns a boolean result based on the
+   *     conditions defined in the rule groups.</li>
+   *     <li>PROBABILISTIC: The engine evaluates rules and returns a probability score based on the
+   *     conditions defined in the rule groups.</li>
+   * </ul>
+   */
+  private final EngineMode engineMode;
+  /**
+   * The minimum probability threshold for the PROBABILISTIC engine mode.
+   * If the calculated probability is below this threshold, the result will be considered false.
+   */
+  private Float minimumProbabilityThreshold;
 
-  public PureRulesEngine(List<RuleGroup<TInput>> ruleGroups) {
-    this.context = new EngineContextImpl();
-    this.ruleGroups = ruleGroups;
+  public static <T> PureRulesEngine<T> getProbablisticEngine(Float minimumProbabilityThreshold, List<RuleGroup<T>> ruleGroups) {
+    return new PureRulesEngine<>(minimumProbabilityThreshold, ruleGroups);
   }
 
-  public PureRulesEngine(List<RuleGroup<TInput>> ruleGroups, EngineMode engineMode) {
-    this.context = new EngineContextImpl();
+  /**
+   * Creates a new instance of PureRulesEngine with the specified rule groups and minimum probability threshold for
+   * PROBABILISTIC mode.
+   * @param minimumProbabilityThreshold - The minimum probability threshold for the PROBABILISTIC engine mode.
+   * @param ruleGroups - The list of rule groups to be evaluated by the engine.
+   */
+  private PureRulesEngine(Float minimumProbabilityThreshold, List<RuleGroup<TInput>> ruleGroups) {
     this.ruleGroups = ruleGroups;
-    if (engineMode != null) {
-      this.engineMode = engineMode;
-    }
+    this.engineMode = EngineMode.PROBABILISTIC;
+    this.minimumProbabilityThreshold = minimumProbabilityThreshold;
+    this.context = new EngineContextImpl();
+  }
+
+  public static <T> PureRulesEngine<T> getDeterministicEngine(List<RuleGroup<T>> ruleGroups) {
+    return new PureRulesEngine<>(ruleGroups);
+  }
+
+  private PureRulesEngine(List<RuleGroup<TInput>> ruleGroups) {
+    this.ruleGroups = ruleGroups;
+    this.engineMode = EngineMode.DETERMINISTIC;
+    this.context = new EngineContextImpl();
   }
 }
