0d31514d44ad1796415a7302877aa08ab2cc80b8
sneakytowelsuit <60020423+sneakytowelsuit@users.noreply.github.com>
2025-07-03 20:18:05 -0700

    Refactor EngineContextService to simplify constructor and remove weight calculation logic

--- src/main/java/com/github/sneakytowelsuit/purerules/context/EngineContextService.java
+++ src/main/java/com/github/sneakytowelsuit/purerules/context/EngineContextService.java
@@ -1,47 +1,20 @@
 package com.github.sneakytowelsuit.purerules.context;
 
-import com.github.sneakytowelsuit.purerules.conditions.Condition;
-import com.github.sneakytowelsuit.purerules.conditions.Rule;
-import com.github.sneakytowelsuit.purerules.conditions.RuleGroup;
 import com.github.sneakytowelsuit.purerules.context.condition.ConditionContext;
 import com.github.sneakytowelsuit.purerules.context.field.FieldContext;
-import com.github.sneakytowelsuit.purerules.engine.EngineMode;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import lombok.Getter;
+
+import java.util.function.Function;
 
 @Getter
 public class EngineContextService<TInput, TInputId> {
   private final ConditionContext<TInputId> conditionEvaluationContext;
   private final FieldContext<TInputId> fieldContext;
-  private final Map<String, Integer> conditionCumulativeWeightMap = new HashMap<>();
   private final Function<TInput, TInputId> inputIdGetter;
 
-  public EngineContextService(
-      EngineMode engineMode,
-      Function<TInput, TInputId> inputIdGetter,
-      List<Condition<TInput>> conditions) {
+  public EngineContextService(Function<TInput, TInputId> inputIdGetter) {
     this.conditionEvaluationContext = new ConditionContext<>();
     this.fieldContext = new FieldContext<>();
     this.inputIdGetter = inputIdGetter;
-    if (engineMode == EngineMode.PROBABILISTIC) {
-      this.calculateWeights(conditions);
-    }
-  }
-
-  private void calculateWeights(List<Condition<TInput>> conditions) {
-    for (Condition<TInput> condition : conditions) {
-      switch (condition) {
-        case Rule<TInput, ?> v -> {
-          this.conditionCumulativeWeightMap.put(v.getId(), v.getCumulativeWeight());
-        }
-        case RuleGroup<TInput> v -> {
-          this.calculateWeights(v.getConditions());
-          this.conditionCumulativeWeightMap.put(v.getId(), v.getCumulativeWeight());
-        }
-      }
-    }
   }
 }
