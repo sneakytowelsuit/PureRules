2142d2ed9a342e1d527282ecd20d71598fd5f2f1
sneakytowelsuit <60020423+sneakytowelsuit@users.noreply.github.com>
2025-07-03 20:16:37 -0700

    Remove redundant constructor from RuleSerializationException for cleaner code

--- src/main/java/com/github/sneakytowelsuit/purerules/exceptions/IncompatibleEvaluationContextException.java
+++
@@ -1,7 +0,0 @@
-package com.github.sneakytowelsuit.purerules.exceptions;
-
-public class IncompatibleEvaluationContextException extends RuntimeException {
-  public IncompatibleEvaluationContextException(String message) {
-    super(message);
-  }
-}
--- src/main/java/com/github/sneakytowelsuit/purerules/exceptions/RuleSerializationException.java
+++ src/main/java/com/github/sneakytowelsuit/purerules/exceptions/RuleSerializationException.java
@@ -1,10 +1,6 @@
 package com.github.sneakytowelsuit.purerules.exceptions;
 
 public class RuleSerializationException extends RuntimeException {
-  public RuleSerializationException(String message) {
-    super(message);
-  }
-
   public RuleSerializationException(String message, Throwable cause) {
     super(message, cause);
   }
